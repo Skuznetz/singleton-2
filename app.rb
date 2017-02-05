7b54eef0c9aa90de535c6adb1044b077b78c3cde
kuznetz <s.kuznetzov@mail.ru>
2017-02-05 20:59:04 +0300

    update logger-2

--- app.rb
+++ app.rb
@@ -1,14 +1,33 @@
 class Logger
+	def initialize
+		@f = File.open 'log.txt', 'a'
+	end
+
+	@@x = nil
+
+	def self.instance
+		if @@x == nil
+			@@x = Logger.new
+		end
+
+		return @@x
+	end
+
 	def self.say_something
 		puts "haha"
     end
 
-    def self.log_something wat
-    	f = File.open 'log.txt', 'a'
-    	f.puts wat
-    	f.close
+    def log_something wat
+    	
+    	@f.puts wat
+    	
     end
 end
  
  Logger.say_something
- Logger.log_something 'wazzup'
+ Logger.instance.log_something 'perviy vizov'
+  Logger.instance.log_something 'drugoy vizov'
+
+ logger = Logger.new
+ logger.log_something 'hey!'
+ 
